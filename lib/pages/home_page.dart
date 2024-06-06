6ef5689f08ad3768961c144c8945fc6afbab3b68
xudayfi <xudayfiibraahim8@gmail.com>
2024-06-06 08:55:17 +0300

    error

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  // Products method
+  Future getProducts() async {
+    var resp = await http.get(Uri.http(''));
+
+    try {
+      if (resp.statusCode == 200) {
+        var data = json.decode(resp.body);
+        print(data);
+      } else {
+        throw Exception('failed');
+      }
+    } catch (e) {
+      print('Error is: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getProducts();
     return const Scaffold();
   }
 }
